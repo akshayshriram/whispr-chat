640f7df26bf00ecf0fbf3ee255781a5ef587ae81
Akshay <akshayshriram999@gmail.com>
2025-12-28 13:40:06 +0530

    Create a Empty Component with Create Room Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
-import Image from "next/image";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { MessageSquareIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="container mx-auto max-w-3xl px-4 py-0 space-y-8">
+        <Empty className="border border-dashed">
+          <EmptyMedia variant={"icon"}>
+            <MessageSquareIcon />
+          </EmptyMedia>
+          <EmptyTitle>No Chat Rooms Found</EmptyTitle>
+          <EmptyDescription>
+            Create a chat room to get started.
+          </EmptyDescription>
+          <EmptyContent>
+            <Button asChild>
+              <Link href="/rooms/new">Create Room</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </div>
+    </>
+  );
 }
