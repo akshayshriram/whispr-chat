b2b83d678d17a6dec4a2254ef64e4e233e05f24b
akshayshriram <akshayshriram999@gmail.com>
2026-01-17 14:40:46 +0530

    Add useRealtimeChat functionality to get the actual user active online

--- app/rooms/[id]/_client.tsx
+++ app/rooms/[id]/_client.tsx
@@ -3,6 +3,9 @@
 import { Message } from "@/services/supabase/actions/messages";
 import ChatInput from "@/components/ChatInput";
 import ChatMessage from "@/components/ChatMessage";
+import { RealtimeChannel } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { createClient } from "@/lib/client";
 
 type ChatRoom = {
     id: string;
@@ -17,13 +20,16 @@ type UserProfile = {
 
 
 export function RoomClient({ room, user, messages }: { room: ChatRoom, user: UserProfile, messages: Message[] }) {
+
+    const { connectedUsers } = useRealtimeChat({ roomId: room.id, userId: user.id })
+
     return (
         <div className="container mx-auto h-screen-with-header border border-y-0 flex flex-col">
             <div className="flex items-center justify-between gap-2">
                 <div className="p-4 border-b">
                     <h1 className="text-2xl font-bold">{room?.name}</h1>
                     {/* Need to add Real-time functionality */}
-                    <p className="text-sm text-muted-foreground">0 users online</p>
+                    <p className="text-sm text-muted-foreground">{connectedUsers} {connectedUsers === 1 ? "user" : "users"} online</p>
                     <p className="text-sm text-muted-foreground">{messages?.length}</p>
                 </div>
 
@@ -51,4 +57,39 @@ function InviteUserModal({ roomId }: { roomId: string }) {
             <h1>Invite User</h1>
         </div>
     )
+}
+
+function useRealtimeChat({ roomId, userId }: { roomId: string, userId: string }) {
+    // const [channel, setChannel] = useState<RealtimeChannel>()
+    const [connectedUsers, setConnectedUsers] = useState<number>(1)
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        const supabase = createClient();
+        const newChannel = supabase.channel(`room:${roomId}:messages`, {
+            config: {
+                private: true,
+                presence: {
+                    key: userId,
+
+                }
+            }
+        })
+        newChannel
+            .on("presence", { event: "sync" }, () => {
+                setConnectedUsers(Object.keys(newChannel.presenceState()).length)
+            })
+            .subscribe(status => {
+                if (status !== "SUBSCRIBED") return;
+
+                newChannel.track({ userId })
+            })
+
+        return () => {
+            newChannel.untrack();
+            newChannel.unsubscribe();
+        }
+    }, [roomId, userId])
+
+    return { connectedUsers, messages }
 }
